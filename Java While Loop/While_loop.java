f9e5297cd0e1b27180ad6dbb62d0fafdaadbbf87
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 00:37:17 -0500

    Create While_loop.java

---
+++ Java While Loop/While_loop.java
@@ -0,0 +1,13 @@
+public class While_loop {
+	public static void main(String args[]) {
+		while (condition) {
+    // code block to be executed
+    }
+    //example
+    int i = 0;
+    while (i < 5) {
+      System.out.println(i);
+      i++;
+    }
+	}
+}
