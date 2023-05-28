07f95636afbc1865ecc641f439f0abe344b9ba95
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 11:20:13 -0500

    add break in while loop

--- Java Break Continue/JavaBreak.java
+++ Java Break Continue/JavaBreak.java
@@ -5,6 +5,16 @@ public class JavaBreak{
         break;
       }
       System.out.println(i);
+			
     }
+	//in while loop
+		int i = 0;
+	while (i < 10) {
+	  System.out.println(i);
+	  i++;
+	  if (i == 4) {
+	    break;
+	  }
+	}
   }
 }
