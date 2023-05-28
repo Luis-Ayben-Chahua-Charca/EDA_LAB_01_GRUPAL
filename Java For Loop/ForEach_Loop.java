fbade95d465b79b63aef8c0163fef6c92a453ed0
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 00:52:43 -0500

    Create ForEach_Loop.java

---
+++ Java For Loop/ForEach_Loop.java
@@ -0,0 +1,12 @@
+public class ForEach_Loop {
+	public static void main(String args[]) {
+		for (type variableName : arrayName) {
+      // code block to be executed
+    }
+    //example
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+    System.out.println(i);
+    }
+  }
+}
