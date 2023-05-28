95dd6a58bb7a53eff913aa27951a8f1962cd4e99
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-05-27 23:48:30 -0500

    commit archivo Java Data Types

---
+++ Java Data Types/Characters.java
@@ -0,0 +1,8 @@
+public class Characters {
+	/*
+	 * El tipo de datos se utiliza para almacenar un solo carácter. El personaje
+	 * debe ser rodeado de comillas simples, como 'A' o 'c':char
+	 */
+	public static void main(String args[]) {
+	}
+}
---
+++ Java Data Types/Non_Primitive_Data_Types.java
@@ -0,0 +1,27 @@
+
+public class Non_Primitive_Data_Types {
+	public static void main(String args[]) {
+		/*
+		 * Los tipos de datos no primitivos se denominan tipos de referencia porque se
+		 * refieren a objetos.
+		 * 
+		 * La principal diferencia entre los tipos de datos primitivos y no primitivos
+		 * son:
+		 * 
+		 * Los tipos primitivos están predefinidos (ya definidos) en Java. Los tipos no
+		 * primitivos son creados por el programador y no está definido por Java
+		 * (excepto para ).String
+		 * Los tipos no primitivos se pueden usar para llamar a métodos para realizar
+		 * ciertas operaciones, mientras que los tipos primitivos no pueden.
+		 * Un tipo primitivo siempre tiene un valor, mientras que los tipos no
+		 * primitivos pueden ser .null
+		 * Un tipo primitivo comienza con una letra minúscula, mientras que los tipos no
+		 * primitivos comienzan con una letra mayúscula.
+		 * El tamaño de un tipo primitivo depende del tipo de datos, mientras que los
+		 * tipos no primitivos tienen todos el mismo tamaño.
+		 * Ejemplos de tipos no primitivos son cadenas, matrices, clases, interfaz, etc.
+		 * Aprenderá más sobre estos en un capítulo posterior.
+		 */
+
+	}
+}
