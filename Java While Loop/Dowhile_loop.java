d7fdd68b6418da7f64734c56c70ace52b49bcbe0
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 00:39:28 -0500

    Create Dowhile_loop.java

---
+++ Java While Loop/Dowhile_loop.java
@@ -0,0 +1,15 @@
+public class Dowhile_loop {
+	public static void main(String args[]) {
+		do {
+      // code block to be executed
+    }
+    while (condition);
+    //example
+    int i = 0;
+    do {
+      System.out.println(i);
+      i++;
+    }
+    while (i < 5);
+	}
+}
