9e5a868599ab9a8e06ed46101440e7b3b276b398
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 11:45:56 -0500

    Create JavaMethods.java

---
+++ Java Methods/JavaMethods.java
@@ -0,0 +1,10 @@
+public class JavaMethods {
+	public static void main(String args[]) {
+    //llamada al metodo
+    myMethod();
+  }
+  //creacion
+  static void myMethod() {
+    // code to be executed
+  }
+}
