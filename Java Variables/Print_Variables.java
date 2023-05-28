23253ed458a018f7582428741fbaaec83c31046e
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-05-27 23:12:25 -0500

    commit archivo Java Variables

---
+++ Java Variables/Print_Variables.java
@@ -0,0 +1,17 @@
+public class Print_Variables {
+	/*
+	 * El método se utiliza a menudo para mostrar variables.println()
+	 * 
+	 * Para combinar texto y una variable, utilice el carácter:+
+	 */
+	public static void main(String args[]) {
+		String firstName = "John ";
+		String lastName = "Doe";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
+
+		int x = 5;
+		int y = 6;
+		System.out.println(x + y);
+	}
+}
---
+++ Java Variables/Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+	public static void main(String args[]) {
+		// type variableName = value;
+		String name = "John";
+		System.out.println(name);
+	}
+}
