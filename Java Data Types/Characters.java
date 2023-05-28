347231ea01222b4b3d41d86112260ba229b1816d
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-05-27 23:54:29 -0500

    commit archivo Java Type Casting y correciones

--- Java Data Types/Characters.java
+++ Java Data Types/Characters.java
@@ -4,5 +4,9 @@ public class Characters {
 	 * debe ser rodeado de comillas simples, como 'A' o 'c':char
 	 */
 	public static void main(String args[]) {
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun); // Outputs true
+		System.out.println(isFishTasty); // Outputs false
 	}
 }
