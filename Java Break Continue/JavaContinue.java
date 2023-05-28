ae08f18c45f204650a358847d8a3c6586925a8c6
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 11:19:10 -0500

    add while loop in continue

--- Java Break Continue/JavaContinue.java
+++ Java Break Continue/JavaContinue.java
@@ -1,10 +1,20 @@
 public class JavaContinue {
-	public static void main(String args[]) {
-		for (int i = 0; i < 10; i++) {
-      if (i == 4) {
-        continue;
-      }
+   public static void main(String args[]) {
+      for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+          continue;
+        }
       System.out.println(i);
-    }
+       }
+	   //in while loop
+	   int i = 0;
+	while (i < 10) {
+	  if (i == 4) {
+	    i++;
+	    continue;
+	  }
+	  System.out.println(i);
+	  i++;
+	}
   }
 }
