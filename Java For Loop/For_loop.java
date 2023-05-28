78bf99f7ecb9cee92799155abdfc2fa27ae42faf
jhuamaniCond <134736830+jhuamaniCond@users.noreply.github.com>
2023-05-28 00:48:39 -0500

    Create For_loop.java

---
+++ Java For Loop/For_loop.java
@@ -0,0 +1,11 @@
+public class For_loop {
+	public static void main(String args[]) {
+		for (statement 1; statement 2; statement 3) {
+     // code block to be executed
+    }
+    //example
+    for (int i = 0; i < 5; i++) {
+     System.out.println(i);
+    }
+	}
+}
